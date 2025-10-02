bae4c656ed1aea4311bd8f5de4aca68e8d88f199
Ryot211 <ryot211@gmail.com>
2025-10-01 21:49:30 -0500

    instalcion morgan y socket.io

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 import express from 'express'
 import logger from 'morgan'
+import {server} from 'socket.io'
+import {createServer} from 'node:http'
 
 const port = process.env.PORT ?? 3000;
 
 
 const app = express()
+const server = createServer(app)
+const io= new Server(server)
+
+io.on('connection',() =>{
+    console.log('a user haz connected!')
+})
+
+
 app.use(logger('dev'))
+
+
 
 app.get('/', (req,res)=> {
     res.sendFile(process.cwd()+'/client/index.html')
 })
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
     console.log(`Server running in port ${port}`)
 })
